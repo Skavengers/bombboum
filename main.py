b5e3867fb57dc310258f3d275d61e546412933ac
Skavengers <91607648+Skavengers@users.noreply.github.com>
2022-06-11 21:53:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from kandinsky import *
+from random import *
 
 def initmap():
-    fill_rect(0,0,320,222,(150,50,0))
+    w=""
+    for i in range(7):
+        w=w+str(randint(0, 1))+str(",")
+    w+=str(randint(0, 1))
+    return w
 
 class Perso:
-    def __init__(self,x,y):
-      self.x=x
-      self.y=y
-    def bomb(self,x,y):
-      pass
+  def __init__(self,x,y):
+    self.x=x
+    self.y=y
+  def bomb(self,x,y):
+    pass
+def fond():
+  c=(0,150,100)
+  fill_rect(0,0,320,222,c)
 def gros():
-  carte1=[[0,1,0,1,0,0,0,0],[0,1,0,1],[0,1,1,1]]
+  carte1=[["0","0","0","1","1","1","0","1","1","1","0","0","0"],["0","0","0","1","1","1","1","1","1","1","0","0","0"] for i in range(8): ,[initmap()]]
   for c in range(len(carte1)):
     for l in range(len(carte1[c])):
       ll=carte1[c][l]
-      if ll==0:
-        fill_rect(40*l,40*c,30,30,(20,)*3)
+      if ll==1:
+        fill_rect(22*l+5,22*c+5,20,20,(20,)*3)
+      else:
+        fill_rect(22*l+5,22*c+5,20,20,(0,100,50))
+
 def loop():
   pass
 gros()
