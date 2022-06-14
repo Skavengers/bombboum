25de951e1e33893efe56b4daa4fe075bd35ae9f0
Skavengers <91607648+Skavengers@users.noreply.github.com>
2022-06-14 18:36:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from kandinsky import *
 from random import *
 
-def initmap():
-    w=""
-    for i in range(7):
-        w=w+str(randint(0, 1))+str(",")
-    w+=str(randint(0, 1))
-    return w
+
+def initmap(li):
+    w = []
+    for i in range(13):
+        w.append(randint(0,1))
+    li.append(w)
+    return li
 
 class Perso:
   def __init__(self,x,y):
@@ -14,13 +15,17 @@ class Perso:
     self.y=y
   def bomb(self,x,y):
     pass
+
+
 def fond():
   c=(0,150,100)
   fill_rect(0,0,320,222,c)
+
 def gros():
-  carte1=[["0","0","0","1","1","1","0","1","1","1","0","0","0"],["0","0","0","1","1","1","1","1","1","1","0","0","0"] for i in range(8): ,[initmap()]]
-  for c in range(len(carte1)):
-    for l in range(len(carte1[c])):
+    carte1=[[0,0,0,1,1,1,0,1,1,1,0,0,0],[0,0,0,1,1,1,1,1,1,1,0,0,0]]
+    for i in range(2):
+        carte=initmap(carte1)  for c in range(len(carte)):
+    for l in range(len(carte[c])):
       ll=carte1[c][l]
       if ll==1:
         fill_rect(22*l+5,22*c+5,20,20,(20,)*3)
