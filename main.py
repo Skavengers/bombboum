3c3e37bca9c7f44dc973c258822c883a1090e49f
Skavengers <91607648+Skavengers@users.noreply.github.com>
2022-06-15 21:17:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,35 @@ def initmap(li):
     return li
 
 class Perso:
-  def __init__(self,x=1,y=1):
+  def __init__(self,x=1,y=1,k=0):
     self.x=x
     self.y=y
+  def mov(self):
+    if p==0:
+      if keydown(KEY_RIGHT):
+        if self.x!=0 :
+          self.x-=20
+        skin()
+      if keydown(KEY_RIGHT):
+        if self.x!=260 :
+          self.x+=20
+        skin()
+      if keydown(KEY_UP):
+        if self.y<=0 :
+          self.y+=20
+        skin()
+      if keydown(KEY_DOWN):
+        if self.x!=0 :
+          self.x-=20
+        skin()
+    
   def bomb(self,x,y):
     pass
 
-  def skin(self):
+  def skin(self,c=(200,200,0)):
     b=(0,)*3
     w=(155,)*3
-    c1=(105,30,50)
-    c2=(255,255,0)
-    fill_rect(self.x+4,self.y+1,20,20,c2)
+    fill_rect(self.x+4,self.y+1,20,20,c)
     fill_rect(self.x+6,self.y+3,4,8,b)
     fill_rect(self.x+18,self.y+3,4,8,b)
     fill_rect(self.x+10,self.y+15,10,4,b)
@@ -45,6 +62,12 @@ def gros():
 p1=Perso(1,1)
 
 def loop():
-  pass
 gros()
 p1.skin()
+while True:
+  draw_string("/bimboum\ ",100,10)
+  draw_string("[1]:play alone",50,50)
+  draw_string("[1]:play in duo",50,80)
+  if keydown(KEY_TWO):
+    p2=Perso(13,1)
+    p2.skin((105,30,50))
